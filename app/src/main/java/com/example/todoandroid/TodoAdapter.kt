f26b57e08d072ae766e8bdf0697d88388567ca02
Omare Faruk <ofaruk0169@gmail.com>
2023-10-31 21:20:38 +0000

    I have coded today, huzzahs are in order

---
+++ app/src/main/java/com/example/todoandroid/TodoAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.todoandroid
+
+import android.graphics.Paint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    private val STRIKE_THRU_TEXT_FLAG = Paint.STRIKE_THRU_TEXT_FLAG
+
+
+    private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean) {
+        if(isChecked) {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags or STRIKE_THRU_TEXT_FLAG
+        } else {
+            tvTodoTitle.paintFlags = tvTodoTitle.paintFlags and STRIKE_THRU_TEXT_FLAG.inv()
+        }
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val curTodo = todos[position]
+        holder.itemView.apply {
+            val tvTodoTitle = findViewById<TextView>(R.id.tvTodoTitle)
+            tvTodoTitle.text = curTodo.title
+
+            val cbDone = findViewById<CheckBox>(R.id.cbDone)
+            cbDone.isChecked = curTodo.isChecked
+
+            toggleStrikeThrough(tvTodoTitle, curTodo.isChecked)
+            cbDone.setOnCheckedChangeListener { _, isChecked ->
+                toggleStrikeThrough(tvTodoTitle, isChecked)
+                curTodo.isChecked = !curTodo.isChecked
+            }
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
